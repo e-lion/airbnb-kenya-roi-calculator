c7fb62f08c88b4f4564736e65efd77c3e2f45abd
Lionel Ngunjiri <lionelngunjiri@gmail.com>
2025-12-24 22:07:33 +0300

    added posthog

--- index.tsx
+++ index.tsx
@@ -8,12 +8,23 @@ if (!rootElement) {
 }
 
 import { BrowserRouter } from 'react-router-dom';
+import { PostHogProvider } from 'posthog-js/react';
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
